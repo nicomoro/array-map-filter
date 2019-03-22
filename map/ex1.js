5a563ce2a034f3a7fa67754b2a615067d557e03f
nico <nicomoro40@gmail.com>
2019-03-22 11:17:22 +0100

    soluce mapEX1

--- map/ex1.js
+++ map/ex1.js
@@ -22,11 +22,15 @@ En sortie: [
  */
 
 function getStringsLength(strings) {
+  const chaine= strings.map(function(n){
+    return `${n} contains ${n.length} characters`;
+  })
+  return chaine;
 }
 
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
 // mais alors, il faut alors lancer l'exercice avec node map/ex1
-// console.log(getStringsLength(['Cat', 'Lion', 'Tiger', 'Jaguar', 'Leopard']));
+console.log(getStringsLength(['Cat', 'Lion', 'Tiger', 'Jaguar', 'Leopard']));
 
 // Ne pas modifier l'export
 module.exports = getStringsLength;
