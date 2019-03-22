4310981b81e4ba62cb54b722f5b0a7c172dffa95
nico <nicomoro40@gmail.com>
2019-03-22 11:55:24 +0100

    soluce ex2

--- map/ex2.js
+++ map/ex2.js
@@ -43,6 +43,15 @@ En sortie: [
  */
 
 function getFoodCategories(foods) {
+  const eat= foods.map(function(food){
+    if(food.isVegetarian===true){
+      return `${foods.food} is suitable for vegetarians`;
+    }
+    else if(food.isVegetarian===false){
+      return `${foods.food} is not suitable for vegetarians`;
+    }
+  })
+  return eat;
 }
 
 
